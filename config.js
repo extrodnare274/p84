d641fe6285e92ed57accd237436c120493cf5b0d
extrodnare274 <76926053+extrodnare274@users.noreply.github.com>
2022-12-04 16:29:31 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCBz7jxeYztSWm6xFc-7owl6cFz_Zy6I-A",
+  authDomain: "storytelling-app-ff217.firebaseapp.com",
+  projectId: "storytelling-app-ff217",
+  storageBucket: "storytelling-app-ff217.appspot.com",
+  messagingSenderId: "113433426559",
+  appId: "1:113433426559:web:8d87d0739387a14b3bdd4a"
+
+};
